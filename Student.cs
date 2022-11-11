d092204156f7c97a8ad9762a56f55e475576a1d4
F_D <den9152@yandex.ru>
2022-11-11 20:58:20 +0700

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Student(int id, string name, string description)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Description = description;
+    }
+    public override string ToString()
+    {
+        return $"Студент {this.Name} ,Возраст  {this.Id}";
+    }
+    public override int GetHashCode()
+    {
+        return this.ToString().GetHashCode();
+    }
+}
---
+++ program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace charp_11_11_2022_hash_code
+{
+    internal class Class1
+    {
+    }
+}
